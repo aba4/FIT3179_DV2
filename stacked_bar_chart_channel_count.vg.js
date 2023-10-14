a4c30a480d6990498e44f3921754e75e3669660c
aba4 <139446352+aba4@users.noreply.github.com>
2023-10-15 02:24:00 +1100

    Update stacked_bar_chart_channel_count.vg.js

--- stacked_bar_chart_channel_count.vg.js
+++ stacked_bar_chart_channel_count.vg.js
@@ -2,7 +2,7 @@
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
   "data": {"url": "https://raw.githubusercontent.com/aba4/FIT3179_DV2/main/cleaned_Youtube_data_1.csv"},
   "transform": [{
-    "calculate": "if(datum.category_figure_ground === 'Other', 0, if(datum.category_figure_ground === 'Entertainment', 1, if(datum.category_figure_ground === 'Music', 2, if(datum.category_figure_ground === 'People & Blogs', 3, if(datum.category_figure_ground === 'Games', 4, 5)))))",
+    "calculate": "if(datum.category_figure_ground === 'Other', 0, if(datum.category_figure_ground === 'Entertainment', 1, if(datum.category_figure_ground === 'Music', 2, if(datum.category_figure_ground === 'People & Blogs', 3, 4))))",
     "as": "categoryOrder"
   }],
   "mark": "bar",
@@ -21,11 +21,15 @@
       "field": "category_figure_ground",
       "type": "nominal",
       "scale": {
-        "domain": ["Entertainment", "Music", "People & Blogs", "Games", "Other"],
-        "scheme": "reds"
+        "domain": ["Entertainment", "Music", "People & Blogs", "Other"],
+        "range": ["#E63D3D","#F17164","#F4B9A2", "#C12727"]
       },
       "title": "Category"
     },
-    "order": {"field": "categoryOrder"}
+    "order":{"field":"categoryOrder"},
+    "tooltip": [
+          {"field": "category_figure_ground", "type": "nominal", "title": "Category"},
+          {"aggregate": "count", "field": "category_figure_ground", "type": "quantitative", "format": ",", "title": "Number of channels"}
+        ]
   }
 }
